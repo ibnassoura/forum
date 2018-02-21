58ac94f0b107349af5af0167464c64b4d2e7ad6e
IbrahimNassoura <ibnassoura@yahoo.com>
2018-02-21 16:49:22 +0400

    Optimizing the code using "for" loop

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -9,25 +9,11 @@ class ATM:
         request=value
         
         if request<=self.balance and request>0:
-            while request>0:
-                if request%100<request:
-                    print "give 100"
-                    request=request-100
-                elif request%50<request:
-                    print "give 50"
-                    request=request-50
-                elif request%10<request:
-                    print "give 10"
-                    request=request-10
-                elif request%5<request:
-                    print "give 5"
-                    request=request-5
-                elif request%2<request:
-                    print "give 2"
-                    request=request-2
-                else:
-                    print "give 1"
-                    request=request-1
+            moneylist=[100,50,10,5,2,1]
+            for each in moneylist:
+                while request%each<request:
+                    print "Give "+str(each)
+                    request=request-each
         else:
             print "Sorry, we dont have enogh money or you entered a wrong number"
             return self.balance
